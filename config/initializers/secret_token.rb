bfa0379a31b39679bc9ed1780e7a2b43004cb593
Leo Wong <gnowoel@gmail.com>
2010-12-08 16:49:24 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xuncheng::Application.config.secret_token = 'ef4408bfe2a3e0ca85d0afd8790a5441075c2f824e3b2114f7200b5c9e7cd0f908b821d25265225c0a7e84b8c2fa5c1702d2f838ceebb294c70a0bae3e1ba736'
