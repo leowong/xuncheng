5a507cdccf85817e92f6ed15166e0066380f5d55
Leo Wong <gnowoel@gmail.com>
2010-12-09 19:59:43 +0800

    adding basic validations for topic

---
+++ spec/factories.rb
@@ -0,0 +1,4 @@
+Factory.define :topic do |topic|
+  topic.title "Hello"
+  topic.content "World!"
+end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -2,10 +2,20 @@ require 'spec_helper'
 
 describe Topic do
   before(:each) do
-    @topic = Topic.new
+    @topic = Factory(:topic)
   end
 
   it "should be valid" do
     @topic.should be_valid
+  end
+
+  describe "validations" do
+    it "should require a topic title" do
+      Topic.new(:title => "", :content => @topic.title).should_not be_valid
+    end
+
+    it "should require nonblank content" do
+      Topic.new(:title => @topic.title, :content => "").should_not be_valid
+    end
   end
 end
