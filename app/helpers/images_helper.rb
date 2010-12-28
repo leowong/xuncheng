31abaefb201e59606111ad4185d49f20bd0dde0d
Leo Wong <gnowoel@gmail.com>
2010-12-28 20:02:09 +0800

    forgot to update image helper methods accordingly

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,6 +1,6 @@
 module ImagesHelper
   def mark_used_images(post)
-    ids = post.content.scan /\[img\].*?\w{2}\/\w{3}\/\w{27}\/(\d+?)\/\w+?\.?\w+?\?\d+?\[\/img\]/
+    ids = post.content.scan /\[img\].*?[\da-f]{2}\/[\da-f]{3}\/[\da-f]{4}\/(\d+?)\/\w+?\.?\w+?\?\d+?\[\/img\]/
     ids.flatten.uniq.each do |id|
       Image.find(id.to_i).update_attributes(:used => true, :post_type => post.class.to_s, :post_id => post.id)
     end
