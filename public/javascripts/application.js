f08b9812fdfc58fc2521d8dc3dc06376b2398ebe
Leo Wong <gnowoel@gmail.com>
2010-12-31 21:40:51 +0800

    adding chinese translations for topics show

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,3 @@
 $(document).ready(function() {
-  $("#upload_images_link").colorbox({innerWidth:"320px", innerHeight:"120px", iframe:true});
+  $("#upload_image_link").colorbox({innerWidth:"320px", innerHeight:"120px", iframe:true});
 });
