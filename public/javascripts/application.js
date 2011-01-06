22a32e103b9a51c86c6de8ed9d2e609d52171f3f
Leo Wong <gnowoel@gmail.com>
2011-01-06 20:00:20 +0800

    adding a reply button for reply cells (not functional yet)

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
   $("#upload_image_link").colorbox({innerWidth:"320px", innerHeight:"120px", iframe:true});
 });
+
+$(document).ready(function() {
+  $(".reply.cell").hover(
+    function() {
+      $(this).addClass("hightlight");
+    },
+    function() {
+      $(this).removeClass("hightlight");
+    }
+  );
+});
