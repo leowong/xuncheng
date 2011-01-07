1c98127ec84ee4fa63f060c92f76ff6641648a6e
Leo Wong <gnowoel@gmail.com>
2011-01-07 14:00:54 +0800

    handling complex usernames when logging @messages

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,4 @@
-# encoding: utf-8
-
 class Post < ActiveRecord::Base
-
   def notify_users
     users_to_notify.each do |user|
       user.notifications << self unless user.notifications.include?(self)
@@ -13,7 +10,7 @@ class Post < ActiveRecord::Base
   def users_to_notify
     text = content
     text += " " + title if type == "Topic"
-    text.scan(/@(.+?)[，。？……！【】（）：；‘’“”——]*\b/).flatten.uniq.map do |name|
+    text.scan(/@(\S+)/).flatten.uniq.map do |name|
       User.find_by_username(name)
     end.select { |user| not user.nil? }
   end
