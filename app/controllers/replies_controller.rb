db3dd3cc910bee775d2ea22c7dba7ce54aaf6c7b
Leo Wong <gnowoel@gmail.com>
2011-01-10 20:45:39 +0800

    synchronizing replies posted by different users

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -3,11 +3,14 @@ class RepliesController < ApplicationController
   include ImagesHelper
 
   def create
+    @last_reply_counter = params[:last_reply_counter]
+    params[:reply].delete(:last_reply_counter)
     @topic = Topic.find(params[:topic_id])
     @reply = current_user.replies.create(params[:reply].merge(:topic_id => @topic.id))
     @reply.update_counter
     @reply.notify_users
     mark_used_images(@reply) if @reply
+    @replies = @topic.replies.order('created_at').where('reply_counter > ?', @last_reply_counter)
 
     respond_to do |format|
       if @reply
