e91acd93644721a79a2baea6f1ce5c20d11f73d2
Leo Wong <gnowoel@gmail.com>
2011-01-24 00:09:07 +0800

    adding validations for noding and grouping

--- app/models/grouping.rb
+++ app/models/grouping.rb
@@ -1,4 +1,7 @@
 class Grouping < ActiveRecord::Base
   belongs_to :user
   belongs_to :node
+
+  validates :user_id, :presence => true
+  validates :node_id, :presence => true
 end
--- app/models/noding.rb
+++ app/models/noding.rb
@@ -1,4 +1,7 @@
 class Noding < ActiveRecord::Base
   belongs_to :node
   belongs_to :topic
+
+  validates :node_id, :presence => true
+  validates :topic_id, :presence => true
 end
